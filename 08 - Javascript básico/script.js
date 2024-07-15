fc6a44bc59913a1db8f5f007c621f08da2abd9f2
Marcos Dhonatha <113925322+marcosdhonatha@users.noreply.github.com>
2024-07-15 12:18:14 -0300

    Update script.js

--- 08 - Javascript básico/script.js
+++ 08 - Javascript básico/script.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+
+
+//############## Aula: Objetos - Propriedades  #############
+
+var aluno = {
+    nome: "Marcos",
+    Nota: [10, 9, 8]
+};
+
+var nomePropriedade = "Idade";
+
+aluno[nomePropriedade] = 21;
+aluno.matricula = 202121;
+aluno.sobrenome = "Dhonatha";
+console.log(aluno);
+
+
+var estudante = new Object();
+estudante.nome = "Marcos";
+estudante.sobrenome = "Dhonatha";
+
+console.log(estudante);
+console.clear();
+//#########################################//
