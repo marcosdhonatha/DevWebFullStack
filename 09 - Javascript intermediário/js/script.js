e4f43f2efdf0c9b9943ad050bc0e7f27c8879823
Marcos Dhonatha <113925322+marcosdhonatha@users.noreply.github.com>
2024-07-19 09:12:43 -0300

    Add files via upload

---
+++ 09 - Javascript intermediário/js/script.js
@@ -0,0 +1,13 @@
+//window.alert("Esse é um alerta")
+
+// Pegando elemento pelo ID
+let listaOrdenada = document.getElementById("lista")
+console.log(listaOrdenada)
+
+// Pegando elemento pelo nome da classe
+let item= document.getElementsByClassName("items")
+console.log(item)
+
+// Pegando elemento pelo nome da tag 
+let items = document.getElementsByTagName("li")
+console.log(items)
