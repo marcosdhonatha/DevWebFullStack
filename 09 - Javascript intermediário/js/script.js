a5503ab7262c5f6c16d84567f4dd7aa3eb8705aa
Marcos Dhonatha <113925322+marcosdhonatha@users.noreply.github.com>
2024-07-23 09:32:29 -0300

    Update script.js

--- 09 - Javascript intermediário/js/script.js
+++ 09 - Javascript intermediário/js/script.js
@@ -11,3 +11,14 @@ console.log(item)
 // Pegando elemento pelo nome da tag 
 let items = document.getElementsByTagName("li")
 console.log(items)
+
+function hiddenButton(button){
+
+    button.style = "display: none"
+}
+
+function changeText(text){
+    let textArea= document.getElementsByClassName("text")[0]
+    textArea.innerText= text
+
+}
